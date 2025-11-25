ab6548260057afa2a12ee6b240200c8693d1d809
Nguyen P. Do <dophucnguyen2005@gmail.com>
2025-11-25 11:26:01 -0600

    Added LoseUI

--- Assets/TimeScript.cs
+++ Assets/TimeScript.cs
@@ -7,19 +7,34 @@ public class TimeScript : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timeText;
     [SerializeField] float remainTime;
+    [SerializeField] GameObject losePanel;
 
+    bool checkLose = false;
     // Update is called once per frame
     void Update()
     {
-        if (remainTime > 0){
-            remainTime -= Time.deltaTime;
+        if (!checkLose){
+            if (remainTime > 0){
+                remainTime -= Time.deltaTime;
+            }
+            else{
+                remainTime = 0;
+                timeText.color = Color.red;
+                lose();
+            }
         }
-        else if (remainTime < 0){
-            remainTime = 0;
-            timeText.color = Color.red;
-        }
+        
         int min = Mathf.FloorToInt(remainTime / 60);
         int sec = Mathf.FloorToInt(remainTime % 60);
         timeText.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+
+    void lose(){
+        checkLose = true;
+        losePanel.SetActive(true);
+
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
