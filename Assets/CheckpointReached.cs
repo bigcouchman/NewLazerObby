c63448f38c47d8c645684c09312f5dc894cb7e44
Nguyen P. Do <dophucnguyen2005@gmail.com>
2025-11-25 22:34:38 -0600

    Added checkpoint object and checkpoint text, fix jump

--- Assets/CheckpointReached.cs
+++ Assets/CheckpointReached.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class CheckpointReached : MonoBehaviour
 {
     public bool isActive = false;
+    public CheckpointPopup checkPop;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Checkpoint hit. checkPop is: " + checkPop);
+
         if (other.CompareTag("Player"))
         {
             CheckpointManager.Instance.SetActiveCheckpoint(this);
+            if (checkPop != null){
+                checkPop.ShowPopUp();
+            }
         }
     }
 
